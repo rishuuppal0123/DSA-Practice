7141a0ff93c73665f91d86fccddb4d2703418660
Rishav Uppal <55997233+rishuuppal0123@users.noreply.github.com>
2022-10-07 18:36:48 +0530

    Create merge sort program

---
+++ C++ Questions/merge_sort.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int p, int q, int r) {
+
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1], M[n2];
+
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[q + 1 + j];
+
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = p;
+  
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
+}
+
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    int mid = l + (r - l) / 2;
+
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid + 1, r);
+
+    merge(arr, l, mid, r);
+  }
+}
+
+int main() {
+  int arr[] = {13, 10, 9, 11, 2, 14, 19, 5};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  mergeSort(arr, 0, size - 1);
+
+  cout << "Sorted array: \n";
+  for(int i=0;i<size;i++)
+    cout<<arr[i]<<" ";
+  cout<<endl;
+  return 0;
+}
